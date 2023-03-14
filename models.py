bd749dc71ed57179ed1024d589b9585f3176fe83
buhmae <erik.buhmann@physik.uni-hamburg.de>
2023-03-14 21:24:05 +0100

    added squash model 2 and weight standard to Linear

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.utils.weight_norm as weight_norm
+from weight_std import Linear_wstd
 
 
 ######################################################################################################
@@ -165,9 +166,12 @@ class EPiC_discriminator_mask(nn.Module):
         x_global = F.leaky_relu(self.dropout(self.fc_g2(x_global)))  # projecting down to latent size
 
         # equivariant connections
+        x_global_in = x_global.clone()
+        x_local_in = x_local.clone()
         for i in range(self.epic_layers):
             # contains residual connection
             x_global, x_local = self.nn_list[i](x_global, x_local, mask)
+            x_global, x_local = x_global + x_global_in, x_local + x_local_in
 
         # again masking global features
         # mean over points dim.
@@ -423,7 +427,7 @@ class EPiC_discriminator_mask_squash(nn.Module):
         for i in range(self.epic_layers):
             # contains residual connection
             x_global, x_local = self.nn_list[i](x_global, x_local, mask)
-            x_global, x_local = x_global+x_global_in, x_local+x_local_in   # skip connection to sampled input
+            # x_global, x_local = x_global+x_global_in, x_local+x_local_in   # skip connection to sampled input
 
         # again masking global features
         # mean over points dim.
@@ -435,4 +439,154 @@ class EPiC_discriminator_mask_squash(nn.Module):
         x = F.leaky_relu(self.fc_g3(x))
         x = F.leaky_relu(self.fc_g4(x) + x)
         x = self.fc_g5(x)
+        return x
+    
+
+
+
+
+#########
+# inspired by FFJORD https://github.com/rtqichen/ffjord/blob/master/lib/layers/diffeq_layers/basic.py
+class ConcatSquashLinear(nn.Module):
+    def __init__(self, dim_in, dim_out, dim_ctx):
+        super().__init__()
+        self._layer = weight_norm(nn.Linear(dim_in, dim_out))
+        self._hyper_bias = weight_norm(nn.Linear(dim_ctx, dim_out, bias=False))
+        self._hyper_gate = weight_norm(nn.Linear(dim_ctx, dim_out))
+
+    def forward(self, ctx, x):
+        gate = torch.sigmoid(self._hyper_gate(ctx))
+        bias = self._hyper_bias(ctx)
+        ret = self._layer(x) * gate + bias
+        return ret
+    
+class ConcatSquashLinear_2inputs(nn.Module):
+    def __init__(self, dim_in, dim_out, dim_ctx1, dim_ctx2):
+        super().__init__()
+        self._layer = weight_norm(nn.Linear(dim_in, dim_out))
+        self._hyper_bias1 = weight_norm(nn.Linear(dim_ctx1, dim_out, bias=False))
+        self._hyper_gate1 = weight_norm(nn.Linear(dim_ctx1, dim_out))
+        self._hyper_bias2 = weight_norm(nn.Linear(dim_ctx2, dim_out, bias=False))
+        self._hyper_gate2 = weight_norm(nn.Linear(dim_ctx2, dim_out))
+
+    def forward(self, ctx1, ctx2, x):
+        gate1 = torch.sigmoid(self._hyper_gate1(ctx1))
+        bias1 = self._hyper_bias1(ctx1)
+        gate2 = torch.sigmoid(self._hyper_gate2(ctx2))
+        bias2 = self._hyper_bias2(ctx2)
+        ret = self._layer(x) * gate1 * gate2 + bias1 + bias2
+        return ret
+
+class EPiC_ConcatSquashLinear(nn.Module):
+    def __init__(self, dim_in, dim_out, dim_ctx, sum_scale=1e-4):
+        super().__init__()
+        self.sum_scale = sum_scale
+        self.act = nn.LeakyReLU()
+        self._layer_ctx = ConcatSquashLinear_2inputs(dim_ctx, dim_ctx, dim_in, dim_in)
+        self.layer = ConcatSquashLinear(dim_in, dim_out, dim_ctx)
+
+    def forward(self, ctx, x, mask):
+        x_sum = (x * mask).sum(1, keepdim=True) * self.sum_scale   # B,1,d
+        x_mean = (x * mask).mean(1, keepdim=True) # B,1,d
+
+        ctx = self.act(self._layer_ctx(x_sum, x_mean, ctx)) # B,1,c
+        ret = self.act(self.layer(ctx, x)) # B,N,d
+        return ctx, ret
+
+
+
+    
+# TODO: maybe make network getting larger and then smaller again
+# DONE: sample aggregation down to context dimension first, then concat
+class EPiC_discriminator_mask_squash2(nn.Module):
+    """EPiC classifier with epic squash layers ONLY (no concat)"""
+
+    def __init__(self, args):
+        """Initialise the EPiC classifier
+
+        Parameters
+        ----------
+        args : keyword argruments
+            Expects:
+                hid_d = dimension of the hidden layers in the phi MLPs
+                feats = number of local features
+                epic_layers = number of epic layers
+                latent = dimension of the latent space (in the networks that act
+                         on the point clouds)
+        """
+        super().__init__()
+        self.hid_d = args.hid_d
+        self.feats = args.feats
+        self.epic_layers = args.epic_layers
+        self.latent = args.latent  # used for latent size of equiv concat
+        self.sum_scale = args.sum_scale
+
+        self.fc_l1 = Linear_wstd(self.feats, self.hid_d)
+        # self.fc_l2 = weight_norm(nn.Linear(self.hid_d, self.hid_d))
+
+        self.fc_g1 = weight_norm(nn.Linear(self.hid_d, self.latent))
+        self.fc_g2 = weight_norm(nn.Linear(self.hid_d, self.latent))
+        self.fc_g3 = weight_norm(nn.Linear(self.latent+self.latent, self.latent))
+
+        self.nn_list = nn.ModuleList()
+        for _ in range(self.epic_layers):
+            self.nn_list.append(
+                EPiC_ConcatSquashLinear(
+                    dim_in=self.hid_d, dim_out=self.hid_d, dim_ctx=self.latent, sum_scale=self.sum_scale
+                )
+            )
+
+        self.fc_g4 = weight_norm(nn.Linear(self.latent, self.hid_d))
+        self.fc_g5 = weight_norm(nn.Linear(self.hid_d, self.hid_d))
+        self.fc_g6 = weight_norm(nn.Linear(self.hid_d, 1))
+
+    def forward(self, x, mask):
+        """Forward propagation through the network
+
+        Parameters
+        ----------
+        x : torch.tensor
+            Input tensor of shape [batch_size, N_points, N_features]
+        mask : torch.tensor
+            Mask of shape [batch_size, N_points, 1]
+            This allows to exclude zero-padded points from the sum/mean aggregation
+            functions
+
+        Returns
+        -------
+        x
+            Output of the network
+        """
+        # local encoding
+        x_local = F.leaky_relu(self.fc_l1(x))
+        # x_local = F.leaky_relu(self.fc_l2(x_local) + x_local)
+
+        # global features: masked
+        # mean over points dim.
+        x_mean = (x_local * mask).mean(1, keepdim=True)  # B,1,d
+        x_mean = self.fc_g1(x_mean) # B,1,C
+        # sum over points dim.
+        x_sum = (x_local * mask).sum(1, keepdim=True) * self.sum_scale # B,1,d
+        x_sum = self.fc_g2(x_sum) # B,1,C
+
+        x_global = torch.cat([x_mean, x_sum], -1) # B,1,C+C
+        x_global = F.leaky_relu(self.fc_g3(x_global))
+
+        x_global_in, x_local_in = x_global.clone(), x_local.clone()
+        # equivariant connections
+        for i in range(self.epic_layers):
+            # contains residual connection
+            x_global, x_local = self.nn_list[i](x_global, x_local, mask)
+            x_global, x_local = x_global+x_global_in, x_local+x_local_in   # skip connection to sampled input
+
+        # again masking global features
+        # mean over points dim.
+        # x_mean = (x_local * mask).mean(1, keepdim=True) # B,1,d
+        # sum over points dim.
+        # x_sum = (x_local * mask).sum(1, keepdim=True) * self.sum_scale   # B,1,d
+        # x = torch.cat([x_mean, x_sum, x_global], -1).reshape(-1, 2 * self.hid_d + self.latent)  
+
+        x = F.leaky_relu(self.fc_g4(x_global))
+        x = F.leaky_relu(self.fc_g5(x) + x)
+        x = self.fc_g6(x)
         return x
--- train.py
+++ train.py
@@ -57,7 +57,9 @@ def main():
     test_loader = DataLoader(test_dataset, batch_size=cfg.batch_size_val, shuffle=False)
 
     # get model
-    model = models.EPiC_discriminator_mask(cfg).to(cfg.device)
+    # model = models.EPiC_discriminator_mask(cfg).to(cfg.device)
+    # model = models.EPiC_discriminator_mask_squash(cfg).to(cfg.device)
+    model = models.EPiC_discriminator_mask_squash2(cfg).to(cfg.device)
     cfg.model_parameters = utils.count_parameters(model)  # count model parameters
     print("Model parameters: ", cfg.model_parameters)
     print(model)
